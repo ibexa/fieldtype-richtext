aacc90f8b41f1568fb24bd39e5be7257160e9b0a
Tomasz Kryszan <tomasz.kryszan@ez.no>
2023-03-03 16:44:31 +0100

    Fixed update query - added nested REPLACE for update value

--- src/lib/Persistence/Legacy/MigrateRichTextNamespaces/Gateway/DoctrineDatabase.php
+++ src/lib/Persistence/Legacy/MigrateRichTextNamespaces/Gateway/DoctrineDatabase.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace Ibexa\FieldTypeRichText\Persistence\Legacy\MigrateRichTextNamespaces\Gateway;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
 use Ibexa\FieldTypeRichText\Persistence\Legacy\MigrateRichTextNamespaces\GatewayInterface;
 
 /**
@@ -17,6 +18,8 @@ use Ibexa\FieldTypeRichText\Persistence\Legacy\MigrateRichTextNamespaces\Gateway
 final class DoctrineDatabase implements GatewayInterface
 {
     private const CONTENT_ATTRIBUTE_TABLE = 'ezcontentobject_attribute';
+
+    private const COLUMN_DATA_TEXT_NAME = 'data_text';
     private const FIELD_TYPE_IDENTIFIER = 'ezrichtext';
 
     private Connection $connection;
@@ -34,21 +37,12 @@ final class DoctrineDatabase implements GatewayInterface
     public function replaceDataTextAttributeValues(array $values): int
     {
         $qb = $this->connection->createQueryBuilder();
-        $qb->update(self::CONTENT_ATTRIBUTE_TABLE);
-
-        $columnName = 'data_text';
-
-        foreach ($values as $oldValue => $newValue) {
-            $oldParam = $qb->createPositionalParameter($oldValue);
-            $newParam = $qb->createPositionalParameter($newValue);
-
-            $qb->set(
-                $columnName,
-                "REPLACE($columnName, $oldParam, $newParam)"
-            );
-        }
-
         $qb
+            ->update(self::CONTENT_ATTRIBUTE_TABLE)
+            ->set(
+                self::COLUMN_DATA_TEXT_NAME,
+                $this->addReplaceStatements($qb, self::COLUMN_DATA_TEXT_NAME, $values)
+            )
             ->andWhere(
                 $qb->expr()->eq(
                     'data_type_string',
@@ -59,5 +53,34 @@ final class DoctrineDatabase implements GatewayInterface
             );
 
         return (int)$qb->execute();
+    }
+
+    /**
+     * @param array<string, string> $values
+     */
+    private function addReplaceStatements(
+        QueryBuilder $queryBuilder,
+        string $columnName,
+        array $values
+    ): string {
+        $replaceStatements = '';
+
+        foreach ($values as $oldValue => $newValue) {
+            $oldParam = $queryBuilder->createPositionalParameter($oldValue);
+            $newParam = $queryBuilder->createPositionalParameter($newValue);
+            $replaceStatements = "REPLACE($columnName, $oldParam, $newParam)";
+
+            if (false !== next($values)) {
+                unset($values[$oldValue]);
+
+                return $this->addReplaceStatements(
+                    $queryBuilder,
+                    $replaceStatements,
+                    $values
+                );
+            }
+        }
+
+        return $replaceStatements;
     }
 }
