825f1bfc6555c5236057a8fec875e78f8eb2364b
Dariusz Szut <dew326@gmail.com>
2023-03-21 14:12:11 +0100

    fixed publising link in image

--- src/bundle/Resources/public/js/CKEditor/link/link-editing.js
+++ src/bundle/Resources/public/js/CKEditor/link/link-editing.js
@@ -74,9 +74,6 @@ class IbexaCustomTagEditing extends Plugin {
         this.editor.model.schema.extend('$text', { allowAttributes: 'ibexaLinkHref' });
         this.editor.model.schema.extend('$text', { allowAttributes: 'ibexaLinkTitle' });
         this.editor.model.schema.extend('$text', { allowAttributes: 'ibexaLinkTarget' });
-        this.editor.model.schema.extend('embedImage', { allowAttributes: 'ibexaLinkHref' });
-        this.editor.model.schema.extend('embedImage', { allowAttributes: 'ibexaLinkTitle' });
-        this.editor.model.schema.extend('embedImage', { allowAttributes: 'ibexaLinkTarget' });
 
         this.defineConverters();
 
