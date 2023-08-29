397ff273f279cf1bb25c8d9e0582218258a2b03b
Dariusz Szut <dew326@gmail.com>
2023-08-29 15:32:41 +0200

    IBX-6313: Fixed anchor in empty paragraph (#125)

--- src/bundle/Resources/public/js/CKEditor/core/base-ckeditor.js
+++ src/bundle/Resources/public/js/CKEditor/core/base-ckeditor.js
@@ -46,7 +46,10 @@ const VIEWPORT_TOP_OFFSET = 102;
         }
 
         getData() {
-            return this.editor.getData();
+            const notTrimmedData = this.editor.getData({ trim: 'none' });
+            const isDataEmpty = notTrimmedData === '<p>&nbsp;</p>';
+
+            return isDataEmpty ? this.editor.getData() : notTrimmedData;
         }
 
         getHTMLDocumentFragment(data) {
