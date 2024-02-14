058d349726399ad07b3cd448f0bca6affd6ac972
Dariusz Szut <dew326@gmail.com>
2024-02-14 09:23:08 +0100

    IBX-6932: Fixed adding custom attrs to list (#139)
    * IBX-6932: Fixed adding custom attrs to list
    
    * Corrected problem with listItem, Added remove custom classes and attributes on new item list
    
    * Corrected clean custom classes
    
    * fix
    
    ---------
    
    Co-authored-by: matx132 <mateusz@mateuszdebinski.pl>

--- src/bundle/Resources/public/js/CKEditor/custom-attributes/custom-attributes-editing.js
+++ src/bundle/Resources/public/js/CKEditor/custom-attributes/custom-attributes-editing.js
@@ -22,8 +22,48 @@ class IbexaCustomAttributesEditing extends Plugin {
             },
         });
 
-        Object.values(customAttributesConfig).forEach((customAttributes) => {
+        Object.entries(customAttributesConfig).forEach(([element, customAttributes]) => {
+            const isList = element === 'ul' || element === 'ol';
+
             Object.keys(customAttributes).forEach((customAttributeName) => {
+                if (isList) {
+                    this.editor.conversion.for('dataDowncast').add((dispatcher) => {
+                        dispatcher.on(`attribute:list-${customAttributeName}:listItem`, (event, data, conversionApi) => {
+                            const viewItem = conversionApi.mapper.toViewElement(data.item);
+
+                            conversionApi.writer.setAttribute(
+                                `data-ezattribute-${customAttributeName}`,
+                                data.attributeNewValue,
+                                viewItem.parent,
+                            );
+                        });
+                    });
+
+                    this.editor.conversion.for('editingDowncast').add((dispatcher) => {
+                        dispatcher.on(`attribute:list-${customAttributeName}:listItem`, (event, data, conversionApi) => {
+                            const viewItem = conversionApi.mapper.toViewElement(data.item);
+
+                            conversionApi.writer.setAttribute(
+                                `data-ezattribute-${customAttributeName}`,
+                                data.attributeNewValue,
+                                viewItem.parent,
+                            );
+                        });
+                    });
+
+                    this.editor.conversion.for('upcast').add((dispatcher) => {
+                        dispatcher.on('element:li', (event, data, conversionApi) => {
+                            const listParent = data.viewItem.parent;
+                            const listItem = data.modelRange.start.nodeAfter || data.modelRange.end.nodeBefore;
+                            const attributeValue = listParent.getAttribute(`data-ezattribute-${customAttributeName}`);
+
+                            conversionApi.writer.setAttribute(`list-${customAttributeName}`, attributeValue, listItem);
+                        });
+                    });
+
+                    return;
+                }
+
                 conversion.attributeToAttribute({
                     model: {
                         key: customAttributeName,
@@ -32,6 +72,55 @@ class IbexaCustomAttributesEditing extends Plugin {
                         key: `data-ezattribute-${customAttributeName}`,
                     },
                 });
+            });
+        });
+
+        this.editor.conversion.for('dataDowncast').add((dispatcher) => {
+            dispatcher.on('attribute:list-custom-classes:listItem', (event, data, conversionApi) => {
+                if (data.attributeKey !== 'list-custom-classes' || data.attributeNewValue === '') {
+                    return;
+                }
+
+                const viewItem = conversionApi.mapper.toViewElement(data.item);
+                const previousElement = viewItem.parent.previousSibling;
+
+                conversionApi.writer.setAttribute('class', data.attributeNewValue, viewItem.parent);
+
+                if (previousElement?.name === viewItem.parent.name) {
+                    conversionApi.writer.mergeContainers(conversionApi.writer.createPositionAfter(previousElement));
+                }
+            });
+        });
+
+        this.editor.conversion.for('editingDowncast').add((dispatcher) => {
+            dispatcher.on('attribute:list-custom-classes:listItem', (event, data, conversionApi) => {
+                if (data.attributeKey !== 'list-custom-classes' || data.attributeNewValue === '') {
+                    return;
+                }
+
+                const viewItem = conversionApi.mapper.toViewElement(data.item);
+                const previousElement = viewItem.parent.previousSibling;
+                const nextElement = viewItem.parent.nextSibling;
+
+                conversionApi.writer.setAttribute('class', data.attributeNewValue, viewItem.parent);
+
+                if (previousElement?.name === viewItem.parent.name) {
+                    conversionApi.writer.mergeContainers(conversionApi.writer.createPositionAfter(previousElement));
+                }
+
+                if (nextElement?.name === viewItem.parent.name) {
+                    conversionApi.writer.mergeContainers(conversionApi.writer.createPositionBefore(nextElement));
+                }
+            });
+        });
+
+        this.editor.conversion.for('upcast').add((dispatcher) => {
+            dispatcher.on('element:li', (event, data, conversionApi) => {
+                const listParent = data.viewItem.parent;
+                const listItem = data.modelRange.start.nodeAfter || data.modelRange.end.nodeBefore;
+                const classes = listParent.getAttribute('class');
+
+                conversionApi.writer.setAttribute('list-custom-classes', classes, listItem);
             });
         });
     }
@@ -44,26 +133,63 @@ class IbexaCustomAttributesEditing extends Plugin {
         }
     }
 
-    init() {
+    cleanAttributes(element, customs) {
         const { model } = this.editor;
+
+        Object.entries(customs).forEach(([elementName, config]) => {
+            if (elementName === element.name) {
+                return;
+            }
+
+            model.change((writer) => {
+                Object.keys(config).forEach((name) => {
+                    writer.removeAttribute(name, element);
+                });
+            });
+        });
+    }
+
+    init() {
+        const { commands, model } = this.editor;
         const customAttributesConfig = getCustomAttributesConfig();
         const customClassesConfig = getCustomClassesConfig();
         const elementsWithCustomAttributes = Object.keys(customAttributesConfig);
         const elementsWithCustomClasses = Object.keys(customClassesConfig);
 
         elementsWithCustomAttributes.forEach((element) => {
+            const isList = element === 'ul' || element === 'ol';
+            const prefix = isList ? 'list-' : '';
+            const elementName = isList ? 'listItem' : element;
             const customAttributes = Object.keys(customAttributesConfig[element]);
 
-            this.extendSchema(model.schema, element, { allowAttributes: customAttributes });
+            customAttributes.forEach((customAttribute) => {
+                this.extendSchema(model.schema, elementName, { allowAttributes: `${prefix}${customAttribute}` });
+            });
         });
 
         elementsWithCustomClasses.forEach((element) => {
-            this.extendSchema(model.schema, element, { allowAttributes: 'custom-classes' });
+            const isList = element === 'ul' || element === 'ol';
+            const prefix = isList ? 'list-' : '';
+            const elementName = isList ? 'listItem' : element;
+
+            this.extendSchema(model.schema, elementName, { allowAttributes: `${prefix}custom-classes` });
         });
 
         this.defineConverters();
 
-        this.editor.commands.add('insertIbexaCustomAttributes', new IbexaCustomAttributesCommand(this.editor));
+        commands.get('enter').on('afterExecute', () => {
+            const blocks = model.document.selection.getSelectedBlocks();
+
+            for (const block of blocks) {
+                this.cleanAttributes(block, customAttributesConfig);
+
+                model.change((writer) => {
+                    writer.removeAttribute('custom-classes', block);
+                });
+            }
+        });
+
+        commands.add('insertIbexaCustomAttributes', new IbexaCustomAttributesCommand(this.editor));
     }
 }
 
