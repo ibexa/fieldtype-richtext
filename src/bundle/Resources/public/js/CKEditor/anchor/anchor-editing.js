272500aa71cae82e4f50f658f5ac7f10125c08f7
Dariusz Szut <dew326@gmail.com>
2024-03-05 14:54:40 +0100

    IBX-7762: Fixed anchor in list

--- src/bundle/Resources/public/js/CKEditor/anchor/anchor-editing.js
+++ src/bundle/Resources/public/js/CKEditor/anchor/anchor-editing.js
@@ -17,6 +17,57 @@ class IbexaAnchorEditing extends Plugin {
                 key: 'id',
             },
         });
+
+        this.editor.conversion.for('dataDowncast').add((dispatcher) => {
+            dispatcher.on('attribute:anchor:listItem', (event, data, conversionApi) => {
+                if (data.attributeKey !== 'anchor' || data.attributeNewValue === '') {
+                    return;
+                }
+
+                const viewItem = conversionApi.mapper.toViewElement(data.item);
+                const previousElement = viewItem.parent.previousSibling;
+
+                conversionApi.writer.setAttribute('id', data.attributeNewValue, viewItem.parent);
+                conversionApi.writer.removeAttribute('id', viewItem);
+
+                if (previousElement?.name === viewItem.parent.name) {
+                    conversionApi.writer.mergeContainers(conversionApi.writer.createPositionAfter(previousElement));
+                }
+            });
+        });
+
+        this.editor.conversion.for('editingDowncast').add((dispatcher) => {
+            dispatcher.on('attribute:anchor:listItem', (event, data, conversionApi) => {
+                if (data.attributeKey !== 'anchor' || data.attributeNewValue === '') {
+                    return;
+                }
+
+                const viewItem = conversionApi.mapper.toViewElement(data.item);
+                const previousElement = viewItem.parent.previousSibling;
+                const nextElement = viewItem.parent.nextSibling;
+
+                conversionApi.writer.setAttribute('id', data.attributeNewValue, viewItem.parent);
+                conversionApi.writer.removeAttribute('id', viewItem);
+
+                if (previousElement?.name === viewItem.parent.name) {
+                    conversionApi.writer.mergeContainers(conversionApi.writer.createPositionAfter(previousElement));
+                }
+
+                if (nextElement?.name === viewItem.parent.name) {
+                    conversionApi.writer.mergeContainers(conversionApi.writer.createPositionBefore(nextElement));
+                }
+            });
+        });
+
+        this.editor.conversion.for('upcast').add((dispatcher) => {
+            dispatcher.on('element:li', (event, data, conversionApi) => {
+                const listParent = data.viewItem.parent;
+                const listItem = data.modelRange.start.nodeAfter || data.modelRange.end.nodeBefore;
+                const classes = listParent.getAttribute('id');
+
+                conversionApi.writer.setAttribute('anchor', classes, listItem);
+            });
+        });
     }
 
     init() {
