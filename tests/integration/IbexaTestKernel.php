f3cb1bc15bb38c20440a875489354030782e32c1
Mikolaj Adamczyk <mikolaj.adamczyk@ez.no>
2024-03-20 11:38:29 +0100

    Added test for translation extraction (#151)

--- tests/integration/IbexaTestKernel.php
+++ tests/integration/IbexaTestKernel.php
@@ -9,18 +9,48 @@ declare(strict_types=1);
 namespace Ibexa\Tests\Integration\FieldTypeRichText;
 
 use DAMA\DoctrineTestBundle\DAMADoctrineTestBundle;
+use Hautelook\TemplatedUriBundle\HautelookTemplatedUriBundle;
+use Ibexa\Bundle\AdminUi\IbexaAdminUiBundle;
+use Ibexa\Bundle\ContentForms\IbexaContentFormsBundle;
+use Ibexa\Bundle\DesignEngine\IbexaDesignEngineBundle;
 use Ibexa\Bundle\FieldTypeRichText\IbexaFieldTypeRichTextBundle;
-use Ibexa\Contracts\Core\Test\IbexaTestKernel as BaseIbexaTestKernel;
+use Ibexa\Bundle\Notifications\IbexaNotificationsBundle;
+use Ibexa\Bundle\Rest\IbexaRestBundle;
+use Ibexa\Bundle\Search\IbexaSearchBundle;
+use Ibexa\Bundle\User\IbexaUserBundle;
 use Ibexa\Contracts\FieldTypeRichText\Persistence\Legacy\MigrateRichTextNamespaces\GatewayInterface;
+use Ibexa\Contracts\Test\Core\IbexaTestKernel as BaseIbexaTestKernel;
 use Ibexa\Core\Persistence\Cache\Identifier\CacheIdentifierGeneratorInterface;
+use Knp\Bundle\MenuBundle\KnpMenuBundle;
+use Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle;
+use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
 use Symfony\Component\Cache\Adapter\TagAwareAdapterInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
 
 final class IbexaTestKernel extends BaseIbexaTestKernel
 {
     public function registerBundles(): iterable
     {
         yield from parent::registerBundles();
+
+        yield new LexikJWTAuthenticationBundle();
+        yield new HautelookTemplatedUriBundle();
+        yield new WebpackEncoreBundle();
+        yield new SwiftmailerBundle();
+        yield new KnpMenuBundle();
+
+        yield new IbexaRestBundle();
+        yield new IbexaContentFormsBundle();
+        yield new IbexaSearchBundle();
+        yield new IbexaUserBundle();
+        yield new IbexaDesignEngineBundle();
+        yield new IbexaAdminUiBundle();
+        yield new IbexaNotificationsBundle();
 
         yield new IbexaFieldTypeRichTextBundle();
         yield new DAMADoctrineTestBundle();
@@ -42,10 +72,25 @@ final class IbexaTestKernel extends BaseIbexaTestKernel
         yield 'ibexa.cache_pool' => TagAwareAdapterInterface::class;
     }
 
-    protected function loadConfiguration(LoaderInterface $loader): void
+    public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        parent::loadConfiguration($loader);
+        parent::registerContainerConfiguration($loader);
 
-        $loader->load(dirname(__DIR__) . '/lib/_settings/common.yaml');
+        $loader->load(__DIR__ . '/../lib/_settings/common.yaml');
+        $loader->load(__DIR__ . '/Resources/config.yaml');
+        $loader->load(static function (ContainerBuilder $container): void {
+            $container->setDefinition(
+                'Symfony\Component\Notifier\NotifierInterface',
+                new Definition(NotifierInterface::class)
+            );
+            $resource = new FileResource(__DIR__ . '/Resources/routing.yaml');
+            $container->addResource($resource);
+            $container->setParameter('form.type_extension.csrf.enabled', false);
+            $container->loadFromExtension('framework', [
+                'router' => [
+                    'resource' => $resource->getResource(),
+                ],
+            ]);
+        });
     }
 }
