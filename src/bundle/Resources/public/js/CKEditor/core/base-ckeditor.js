1d3e3d67c5f1862e0fffdb1863db681f2f5669e0
Thorsten Reiter <thorsten.reiter@ibexa.co>
2024-07-09 11:53:12 +0200

    run yarn prettier

--- src/bundle/Resources/public/js/CKEditor/core/base-ckeditor.js
+++ src/bundle/Resources/public/js/CKEditor/core/base-ckeditor.js
@@ -126,10 +126,10 @@ const VIEWPORT_TOP_OFFSET_DISTRACTION_FREE_MODE = 0;
             const section = wrapper.childNodes[0];
             const { toolbar, extraPlugins = [], extraConfig = {} } = window.ibexa.richText.CKEditor;
             let locale;
-            try{
-                locale  = new Intl.Locale(doc.querySelector('meta[name="LanguageCode"]').content);
-            }catch (e){
-                locale  = new Intl.Locale('eng-GB');
+            try {
+                locale = new Intl.Locale(doc.querySelector('meta[name="LanguageCode"]').content);
+            } catch (e) {
+                locale = new Intl.Locale('eng-GB');
             }
             const blockCustomStyles = Object.entries(ibexa.richText.customStyles)
                 .filter(([, customStyleConfig]) => !customStyleConfig.inline)
