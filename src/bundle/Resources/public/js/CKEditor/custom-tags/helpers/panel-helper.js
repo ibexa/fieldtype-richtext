e3a3fc6f4bd354e2dbcbb0030929c026e16cca1d
Lukasz Ostafin <ostafin.lucas@gmail.com>
2024-09-17 14:50:06 +0200

    After QA

--- src/bundle/Resources/public/js/CKEditor/custom-tags/helpers/panel-helper.js
+++ src/bundle/Resources/public/js/CKEditor/custom-tags/helpers/panel-helper.js
@@ -1,4 +1,5 @@
 const setPanelContentMaxHeight = (balloonView) => {
+    const HEADER_HEIGHT = 90;
     const { innerHeight: windowHeight } = window;
     const { top: panelTopPosition, element: panelNode } = balloonView;
     const panelHeader = panelNode.querySelector('.ibexa-custom-tag-panel-header');
@@ -11,7 +12,7 @@ const setPanelContentMaxHeight = (balloonView) => {
 
     const panelHeaderHeight = panelHeader?.offsetHeight ?? 0;
     const panelFooterHeight = panelFooter?.offsetHeight ?? 0;
-    const isPanelOverTopWindowEdge = panelTopPosition < 0;
+    const isPanelOverTopWindowEdge = panelTopPosition - HEADER_HEIGHT < 0;
     const maxHeightValue = isPanelOverTopWindowEdge
         ? panelContent.offsetHeight - Math.abs(panelTopPosition)
         : windowHeight - panelTopPosition - panelHeaderHeight - panelFooterHeight;
