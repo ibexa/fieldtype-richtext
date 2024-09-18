6b6225b7e33a84393261ad23e5a3226616ca0e05
Lukasz Ostafin <ostafin.lucas@gmail.com>
2024-09-18 14:11:36 +0200

    After QA

--- src/bundle/Resources/public/js/CKEditor/custom-tags/block-custom-tag/custom-tag-ui.js
+++ src/bundle/Resources/public/js/CKEditor/custom-tags/block-custom-tag/custom-tag-ui.js
@@ -19,6 +19,22 @@ class IbexaCustomTagUI extends Plugin {
         this.addCustomTag = this.addCustomTag.bind(this);
 
         this.isNew = false;
+        this.lastClassListValue = null;
+        this.observer = new MutationObserver((mutationList) => {
+            for (const mutation of mutationList) {
+                if (this.lastClassListValue !== mutation.target.classList.value) {
+                    if (this.lastClassListValue !== null) {
+                        setPanelContentMaxHeight(this.balloon.view);
+                    }
+
+                    this.lastClassListValue =  mutation.target.classList.value;
+                }
+              }
+        });
+
+        this.observer.observe(this.balloon.view.element, {
+            attributes: true,
+        });
     }
 
     isCustomTagSelected(eventData) {
