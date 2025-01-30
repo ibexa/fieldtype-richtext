ae5e0d6dae45f08237b941157ce3cf3c4661ffa8
Michał Grabowski <michal.grabowski@ibexa.co>
2025-01-30 11:59:07 +0100

    upcast for class in image link

--- src/bundle/Resources/public/js/CKEditor/embed/image/embed-image-editing.js
+++ src/bundle/Resources/public/js/CKEditor/embed/image/embed-image-editing.js
@@ -213,6 +213,7 @@ class IbexaEmbedImageEditing extends Plugin {
                     upcastWriter.setAttribute('ibexaLinkHref', link.getAttribute('href'), modelElement);
                     upcastWriter.setAttribute('ibexaLinkTitle', link.getAttribute('title') ?? '', modelElement);
                     upcastWriter.setAttribute('ibexaLinkTarget', link.getAttribute('target') ?? '', modelElement);
+                    upcastWriter.setAttribute('ibexaLinkClasses', link.getAttribute('class') ?? '', modelElement);
                 }
 
                 return modelElement;
