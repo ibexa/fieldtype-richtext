76259006ac325a1daa51a9d7af5ff17b00275910
Michał Grabowski <michal.grabowski@ibexa.co>
2025-06-03 16:00:37 +0200

    removed circular aliases

--- src/bundle/Resources/public/js/CKEditor/formatted/formatted-ui.js
+++ src/bundle/Resources/public/js/CKEditor/formatted/formatted-ui.js
@@ -21,7 +21,7 @@ class IbexaFormattedUI extends Plugin {
 
             buttonView.set({
                 label: Translator.trans(/*@Desc("Formatted")*/ 'formatted_btn.label', {}, 'ck_editor'),
-                icon: window.ibexa.helpers.icon.getIconPath('tag'),
+                icon: window.ibexa.helpers.icon.getIconPath('text-code'),
                 tooltip: true,
             });
 
