6a8781ba302d7f6667734c24e717cbb699ed7738
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2026-05-04 13:32:23 +0200

    Regenerated PHPStan

--- ignore-by-php-version.neon.php
+++ ignore-by-php-version.neon.php
@@ -7,13 +7,9 @@
 declare(strict_types=1);
 
 $includes = [];
-if (PHP_VERSION_ID < 80000) {
+if (PHP_VERSION_ID < 8_00_00) {
     $includes[] = __DIR__ . '/ignore-lte-php7.4-errors.neon';
-} else {
-    $includes[] = __DIR__ . '/ignore-gte-php8.0-errors.neon';
-}
-
-if (PHP_VERSION_ID > 80100) {
+} else  {
     $includes[] = __DIR__ . '/ignore-gte-php8.1-errors.neon';
 }
 
