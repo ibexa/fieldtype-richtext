0fb9e63330da9d7042a1b2a4d18b647e2bf4f9cf
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2026-09-01 09:55:19 +0200

    IBX-12358: Introduced Bootstrapper / Hooks for integration tests

--- tests/integration/bootstrap.php
+++ tests/integration/bootstrap.php
@@ -6,42 +6,8 @@
  */
 declare(strict_types=1);
 
-use Ibexa\Contracts\Core\Test\Persistence\Fixture\FixtureImporter;
-use Ibexa\Tests\Core\Repository\LegacySchemaImporter;
-use Ibexa\Tests\Integration\FieldTypeRichText\IbexaTestKernel;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
+use Ibexa\Contracts\Test\Core\Bootstrapper\Bootstrapper;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
-$kernel = new IbexaTestKernel('test', true);
-$kernel->boot();
-
-$application = new Application($kernel);
-$application->setAutoExit(false);
-
-$databaseUrl = getenv('DATABASE_URL');
-if ($databaseUrl !== false && strpos($databaseUrl, 'sqlite') !== 0) {
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:drop',
-        '--if-exists' => '1',
-        '--force' => '1',
-    ]));
-}
-
-$application->run(new ArrayInput([
-    'command' => 'doctrine:database:create',
-]));
-
-/** @var \Psr\Container\ContainerInterface $testContainer */
-$testContainer = $kernel->getContainer()->get('test.service_container');
-
-$schemaImporter = $testContainer->get(LegacySchemaImporter::class);
-foreach ($kernel->getSchemaFiles() as $file) {
-    $schemaImporter->importSchema($file);
-}
-
-$fixtureImporter = $testContainer->get(FixtureImporter::class);
-foreach ($kernel->getFixtures() as $fixture) {
-    $fixtureImporter->import($fixture);
-}
+(new Bootstrapper())->bootstrap();
